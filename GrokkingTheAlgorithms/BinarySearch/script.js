0ba911a568d1379cd79d76bb805ce124fd523bd9
Sam Jeffcoat <samjeffcoat@gmail.com>
2020-05-19 16:31:44 -0500

    binary search done in python and JS

---
+++ GrokkingTheAlgorithms/BinarySearch/script.js
@@ -0,0 +1,17 @@
+function binarySearch(list, item) {
+  let low = 0;
+  let high = list.length;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = list[mid];
+    if (guess === item) {
+      return true;
+    }
+    if (guess < item) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return false;
+}
