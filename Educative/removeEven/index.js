9738c32ab49bf7edda37a3d2513094a44cf18f6f
Sam Jeffcoat <samjeffcoat@gmail.com>
2020-06-17 14:00:10 -0500

    array1

---
+++ Educative/removeEven/index.js
@@ -0,0 +1,16 @@
+// Solution 1
+
+function removeEven(arr) {
+  var odds = [];
+  for (let number of arr) {
+    if (number % 2 != 0)
+      // check if our indexes our divisible by 2, if they arent
+      odds.push(number); // push them to our new array
+  }
+  return odds;
+}
+//using filter and lambda function
+
+function removeEven2(arr) {
+  return arr.filter((t) => t % 2 != 0);
+}
