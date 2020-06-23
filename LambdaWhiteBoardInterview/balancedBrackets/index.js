fb75fc7f9226e6eb52707e616694fba313d41b8d
Sam Jeffcoat <samjeffcoat@gmail.com>
2020-06-23 12:19:45 -0500

    testing

--- LambdaWhiteBoardInterview/balancedBrackets/index.js
+++ LambdaWhiteBoardInterview/balancedBrackets/index.js
@@ -1,15 +1,32 @@
 function balancedBrackets(string) {
-  const openingBrackets = '([{|';
+  //intialize string of all opening brackets
+  const openingBrackets = '([{';
+  // intialize string of all closing brackets
   const closingBrackets = ')]}|';
+
+  const onlyMatchingBracket = '|';
+  // hashtable, map closing brackets to opening brackets
   const matchingBrackets = { ')': '(', ']': '[', '}': '{', '|': '|' };
+
+  // initialize our stack to be empty array
   const stack = [];
+
+  //check if we have opening brackets
+  // add all opening brackets to stack
+
   for (const char of string) {
     if (openingBrackets.includes(char)) {
       stack.push(char);
+
+      // if current charachter is closing bracket
+      // if nothing is in the stack then we return false since there are no brackets to map to
     } else if (closingBrackets.includes(char)) {
       if (stack.length == 0) {
         return false;
       }
+      //check  to see last value in our in our closing bracket maps to opening bracket
+
+      //pop off that value of the stack
       if (stack[stack.length - 1] === matchingBrackets[char]) {
         stack.pop();
       } else {
@@ -19,3 +36,12 @@ function balancedBrackets(string) {
   }
   return stack.length === 0;
 }
+
+console.log(balancedBrackets('||({{}})'));
+console.log(balancedBrackets('{}'));
+console.log(balancedBrackets('||({{}})]'));
+// console.log(balancedBrackets('||'));
+// console.log(balancedBrackets('||'));
+// console.log(balancedBrackets('||'));
+// console.log(balancedBrackets('||'));
+// console.log(balancedBrackets('||'));
